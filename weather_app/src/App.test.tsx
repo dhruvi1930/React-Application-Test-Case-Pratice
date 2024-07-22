58baa1529fc5aa99ffc48b621b4b7802438f4da0
Dhruvi <dhruvihl369@gmail.com>
2024-07-22 19:31:51 -0400

    added more test cases

--- weather_app/src/App.test.tsx
+++ weather_app/src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 // render application title
 test("renders title weather app", () => {
@@ -14,4 +15,19 @@ test("render input filed and button", () => {
   render(<App />);
   screen.getByPlaceholderText("Enter City");
   screen.getByRole("button");
+});
+
+// user can enter city name and print weather
+test("render api call function", async () => {
+  render(<App />);
+  const inputElement: HTMLInputElement =
+    screen.getByPlaceholderText("Enter City");
+  const buttonElement: HTMLButtonElement = screen.getByRole("button");
+
+  await userEvent.type(inputElement, "Waterloo");
+  expect(inputElement).toHaveValue("Waterloo");
+
+  await userEvent.click(buttonElement);
+  expect(inputElement).toHaveValue("");
+  screen.getByText("Weather");
 });
--- weather_app/src/App.tsx
+++ weather_app/src/App.tsx
@@ -1,27 +1,54 @@
-import { useEffect } from "react";
+import React, { useCallback, useState } from "react";
 import "./App.css";
 
+interface Weather {
+  temp: number;
+  speed: number;
+  humidity: number;
+}
+
 function App() {
-  useEffect(() => {
-    const fetchWeather = async () => {
-      const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+  const [city, setCity] = useState<string>("");
+  const [weather, setWeather] = useState<Weather | null>(null);
 
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=surat&appid=${apiKey}`
-      );
-
-      const data = await response.json();
-      console.log(data);
-    };
-
-    fetchWeather();
+  const fahrenheitToCelsius = useCallback((temp: number) => {
+    return temp - 273.15;
   }, []);
+
+  const handleFetchData = useCallback(async () => {
+    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    );
+
+    const data = await response.json();
+    const tempInCelsius: number = fahrenheitToCelsius(data.main.temp);
+
+    setWeather({
+      temp: tempInCelsius,
+      speed: data.wind.speed,
+      humidity: data.main.humidity,
+    });
+    setCity("");
+  }, [city, fahrenheitToCelsius]);
 
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <input placeholder="Enter City" />
-      <button>Search</button>
+      <input
+        placeholder="Enter City"
+        value={city}
+        onChange={(event) => setCity(event.target.value)}
+      />
+      <button onClick={handleFetchData}>Search</button>
+      {weather && (
+        <div>
+          <p>Weather: {weather.temp.toFixed(2)}</p>
+          <p>Speed: {weather.speed}</p>
+          <p>Humidity: {weather.humidity}</p>
+        </div>
+      )}
     </div>
   );
 }
